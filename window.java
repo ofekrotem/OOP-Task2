4b21ea72640622be633e6c74065929d671ab8ce9
YoniTal6899 <89694679+YoniTal6899@users.noreply.github.com>
2021-12-09 15:16:40 +0200

    Add files via upload
    create empty window

---
+++ window.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class window extends JFrame
+{
+    public window()
+    {
+        setTitle("Ex2 graphs");
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(700,600);
+    }
+}
